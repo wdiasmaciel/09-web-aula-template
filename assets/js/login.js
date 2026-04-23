722ef16383914eb2db8d7c814b7406ae178071ce
Wesley Dias Maciel <wesleydiasmaciel@gmail.com>
2026-04-23 02:17:26 -0300

    Exemplo

---
+++ assets/js/login.js
@@ -0,0 +1,15 @@
+const email = "admin@exemplo.com";
+const senha = "1234";
+
+function verificarCredenciais() {
+  const emailInformado = document.getElementById("email").value;
+  const senhaInformada = document.getElementById("senha").value;
+
+  if (emailInformado === email) {
+    alert("E-mail informado corretamente!");
+    if (senhaInformada === senha) {
+      alert("Senha informada corretamente!");
+      window.location = "home.html";
+    } else alert("Senha informada incorretamente!");
+  } else alert("E-mail informado incorretamente!");
+}
